84951c0138c5a25e40944355b88156c7d41c8cc2
NoCodeForYou <jmans.gamenetwork@gmail.com>
2025-09-08 09:51:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    base: "/UTXO/",
     const env = loadEnv(mode, '.', '');
     return {
       define: {
